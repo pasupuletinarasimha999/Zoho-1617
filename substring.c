5489bb0201606a38725482e9f1b619d02d043cbb
Narasimha P <pasupuletinarasimha999@gmail.com>
2017-01-21 15:37:47 +0530

    Update substring.c

--- substring.c
+++ substring.c
@@ -1,20 +1,22 @@
 # Zoho-1617
 #include <stdio.h>
-#include<string.h>
- 
-char string[50], n;
+
 int main()
 {
-    int i, len,j,index,start,n;
-    printf("Enter the string ");
-    scanf("%s", string);
-    len=strlen(string);
+    int i, len,j,index,start,n,a[100];
+    printf("enter the number of elements \n" );
+    scanf("%d",&len);
+    printf("Enter the elements one by one ");
+    for(i=0;i<len;i++)
+    {
+    	scanf("%d",&a[i]);
+    }
     start=0;
     index=1;
     printf("The subsets are :\n");
     for(i=0;i<len;i++)
     {
-    	printf("%c \n",string[i]);
+    	printf("%d \n",a[i]);
     }
     while(n!=len)
     {
@@ -24,10 +26,10 @@ int main()
 	    	{
 	    		for(j=start;j<index;j++)
 	    		{
-		    		printf("%c",string[j]);
+		    		printf("%d,",a[j]);
 		    	}
 		    	
-		    	printf("%c \n",string[i]);
+		    	printf("%d \n",a[i]);
 	    	}
 	    	index++;
 	    }
